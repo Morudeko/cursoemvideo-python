d25e1dac69582370a23af938096135657ab6230b
Gabriel Mordecai <39249349+Morudeko@users.noreply.github.com>
2020-04-22 17:43:30 -0300

    ex003
    Fiz algumas alterações, no caso eu imbuti a soma na função print e dentro das chaves coloquei nomeclaturas que facilitam a troca da ordem das informações.

--- mundo-01/exercícios/ex003.py
+++ mundo-01/exercícios/ex003.py
@@ -1,4 +1,13 @@
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
-s = n1 + n2
-print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
+# Forma mais simples
+# n1 = int(input('Digite um valor: '))
+# n2 = int(input('Digite outro valor: '))
+# s = n1 + n2
+# print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
+
+# Forma um pouco mais complicada
+# Variáveis
+numum = int(input("Digite um número: "))
+numdois = int(input("Digite um número: "))
+
+# Função print com soma imbutida
+print("A soma entre {num} e {ndois} é igual à {res}".format(num=numum, ndois=numdois, res=numum + numdois))
